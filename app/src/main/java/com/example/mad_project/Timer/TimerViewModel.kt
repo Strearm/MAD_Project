e0cfc3086ead9781f96ad22f7c25c688c7b94c94
Strearm <92020515+Strearm@users.noreply.github.com>
2023-05-18 15:05:01 +0200

    update timer and timerViewModel v1

--- app/src/main/java/com/example/mad_project/Timer/TimerViewModel.kt
+++ app/src/main/java/com/example/mad_project/Timer/TimerViewModel.kt
@@ -2,37 +2,90 @@ package com.example.mad_project.Timer
 
 import androidx.compose.runtime.MutableState
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.delay
 
 class TimerViewModel :ViewModel(){
-
-    private val _buttonState = false
+    //region UIVariables
+    private var _buttonState = false
     val buttonState:Boolean
         get() = _buttonState
 
-    fun toggleStart(){
-        _buttonState != _buttonState
-    }
+    private var _locked = false
+    val locked:Boolean
+        get() = _locked
 
-    fun checkInput(value: String):Boolean{
-        return value.toIntOrNull() == null
+    private var _hours = "00"
+    val hours: String
+        get() = _hours
+
+    private var _minutes = "00"
+    val minutes:String
+        get() = _minutes
+
+    private var _seconds = "00"
+    val seconds:String
+        get() = _seconds
+    //endregion UIVariables
+    private var lastProperInput: String = "00"
+    fun toggleStart(){ _buttonState = !_buttonState }
+    fun toFalse(){ _buttonState = false }
+    fun updateHours(hour: String){
+        _hours = checkUnder24(hour)
+    }
+    fun updateMin(minute: String){
+        _minutes = checkUnder60(minute)
+    }
+    fun updateSec(second: String){
+        _seconds = checkUnder60(second)
     }
     fun checkUnder60(value:String): String{
-
-        return if(value.toIntOrNull() == null){
+        return if(value.isEmpty()){
             "00"
+        }else if(value.toIntOrNull() == null){
+            lastProperInput
         } else if(value.toInt() >= 60){
             "59"
         } else{
+            lastProperInput = value
             value
         }
     }
     fun checkUnder24(value:String): String{
-        return if(value.toIntOrNull()== null){
+        return if(value.isEmpty()){
             "00"
-        } else if(value.toInt() > 24 ){
+        }else if(value.toIntOrNull() == null){
+            lastProperInput
+        } else if(value.toInt() >= 24){
             "24"
         } else{
+            lastProperInput = value
             value
+        }
+    }
+    suspend fun countDown(){
+        var currentSec = _seconds.toInt()
+        var currentMin = _minutes.toInt()
+        var currentH = _hours.toInt()
+
+        while (currentSec >= 0 && buttonState) {
+            _seconds = String.format("%02d", currentSec)
+            _minutes = String.format("%02d", currentMin)
+            _hours = String.format("%02d", currentH)
+            currentSec -= 1
+            delay(timeMillis = 1000L)
+            if(currentMin > 0 && currentSec < 0){
+                currentMin -= 1
+                currentSec = 59
+            }else if (currentH > 0 && currentMin == 0 && currentSec < 0){
+                currentH -= 1
+                currentMin = 59
+                currentSec = 59
+            }else if(currentH <= 0 && currentMin <= 0 && currentSec <= 0){
+                _hours = "00"
+                _minutes = "00"
+                _seconds = "00"
+                toggleStart()
+            }
         }
     }
 }
