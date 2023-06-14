e14ee6355b987f38279f1837a708895d7ef5c440
Strearm <92020515+Strearm@users.noreply.github.com>
2023-06-14 11:55:40 +0200

    timer keeps running even when screen rotated

--- app/src/main/java/com/example/mad_project/Timer/TimerScreen.kt
+++ app/src/main/java/com/example/mad_project/Timer/TimerScreen.kt
@@ -1,6 +1,11 @@
 package com.example.mad_project.Timer
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Button
 import androidx.compose.material.LocalTextStyle
 import androidx.compose.material.OutlinedTextField
@@ -9,149 +14,66 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import com.example.mad_project.widgets.SimpleTopAppBar
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
-import kotlin.concurrent.timer
-
+var globSec: String = "00"
+var globMin: String = "00"
+var globH: String = "00"
 @Composable
 fun TimerScreen(navController: NavHostController) {
     //region variable declarations
-    //region time variables
-    var hours by rememberSaveable { mutableStateOf("00") }
-    var minutes by rememberSaveable { mutableStateOf("00") }
-    var seconds by rememberSaveable { mutableStateOf("00") }
-    var currentSec = seconds.toInt()
-    var currentMin = minutes.toInt()
-    var currentH = hours.toInt()
-    //endregion time variables
-
-    var startButtonState by rememberSaveable{
-        mutableStateOf(false)
-    }
-    var buttonEnable by rememberSaveable{
-        mutableStateOf(false)
-    }
     val coroutineScope = rememberCoroutineScope()
     val timerViewModel: TimerViewModel = viewModel()
+    val startButtonState by timerViewModel.buttonState.collectAsState()
+
+    val Sec by timerViewModel.seconds.collectAsState()
+    val Min by timerViewModel.minutes.collectAsState()
+    val Hour by timerViewModel.hours.collectAsState()
     //endregion variable declarations
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-
         Column(
             modifier = Modifier.fillMaxWidth(),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            SimpleTopAppBar(arrowBackClicked = { navController.popBackStack() }, navController = navController
+            //TopBAR
+            SimpleTopAppBar(arrowBackClicked = { navController.popBackStack() },
+                navController = navController
             ) {
                 Text(text = "Timer")
             }
-
             Column(
                 modifier = Modifier.weight(weight = 9f),
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Center,
             )
             {
-                Row(
-                    modifier = Modifier.padding(15.dp)
-                ) {
-                    OutlinedTextField(
-                        modifier = Modifier.weight(3f),
-                        value = hours,
-                        onValueChange = { timerViewModel.updateHours(it)
-                                            hours = timerViewModel.hours
-                                        },
-                        singleLine = true,
-                        readOnly = startButtonState,
-                        label = { Text(text = "Hours") },
-                        textStyle = LocalTextStyle.current.copy(textAlign = TextAlign.End),
-                    )
-                    OutlinedTextField(
-                        modifier = Modifier.weight(3f),
-                        value = minutes,
-                        onValueChange = { timerViewModel.updateMin(it)
-                                            minutes = timerViewModel.minutes
-                                        },
-                        singleLine = true,
-                        readOnly = startButtonState,
-                        label = { Text(text = "Minutes") },
-                        textStyle = LocalTextStyle.current.copy(textAlign = TextAlign.End),
-                    )
-                    OutlinedTextField(
-                        modifier = Modifier.weight(3f),
-                        value = seconds,
-                        onValueChange = { timerViewModel.updateSec(it)
-                                            seconds = timerViewModel.seconds
-                                        },
-                        singleLine = true,
-                        readOnly = startButtonState,
-                        label = { Text(text = "Seconds") },
-                        textStyle = LocalTextStyle.current.copy(textAlign = TextAlign.End),
-                    )
-                }
-                //inputFields()
-                Clock(hour = timerViewModel.hours, minute = timerViewModel.minutes, second = timerViewModel.seconds)
+                //Main Content
+                InputFields(timerViewModel = timerViewModel, startButtonState = startButtonState)
+                //RealTimeCountdown
+                Clock(hour = Hour, minute = Min, second = Sec)
             }
             Row(modifier = Modifier.weight(1f)) {
-                buttonEnable = currentH > 0 || currentMin > 0 || currentSec > 0
-                Button(modifier = Modifier
-                    .weight(1f)
-                    .padding(15.dp),
-                    enabled = buttonEnable,
-                    onClick = {
-                        startButtonState = !startButtonState
-                        coroutineScope.launch {
-                            while (currentSec >= 0 && startButtonState) {
-                                seconds = String.format("%02d", currentSec)
-                                minutes = String.format("%02d", currentMin)
-                                hours = String.format("%02d", currentH)
-                                currentSec -= 1
-                                delay(timeMillis = 1000L)
-                                if(currentMin > 0 && currentSec < 0){
-                                    currentMin -= 1
-                                    currentSec = 59
-                                }else if (currentH > 0 && currentMin == 0 && currentSec < 0){
-                                    currentH -= 1
-                                    currentMin = 59
-                                    currentSec = 59
-                                }else if(currentH <= 0 && currentMin <= 0 && currentSec <= 0){
-                                    hours = "00"
-                                    minutes = "00"
-                                    seconds = "00"
-                                    startButtonState = !startButtonState
-                                }
-                            }
-                        }
-                    }
-                ){
-                    Text(text = if (startButtonState) "Stop" else "Start")
-                }
-                Spacer(modifier = Modifier.width(10.dp))
-                Button(modifier = Modifier
-                    .weight(1f)
-                    .padding(15.dp),
-                    onClick = {
-                        startButtonState = false
-                        hours = "00"
-                        minutes = "00"
-                        seconds = "00"
-                    }) {
-                    Text(text = "Reset")
-                }
+                //buttonRow
+                ButtonRow(
+                    timerViewModel = timerViewModel,
+                    startButtonState = startButtonState,
+                    coroutineScope = coroutineScope
+                )
             }
         }
     }
 }
-
 
 @Composable
 fun Clock(hour: String, minute: String, second: String) {
@@ -162,43 +84,91 @@ fun Clock(hour: String, minute: String, second: String) {
 }
 
 @Composable
-fun inputFields(){
-    var inputHours by rememberSaveable {
-        mutableStateOf("")
-    }
-    var inputMinutes  by rememberSaveable {
-        mutableStateOf("")
-    }
-    var inputSeconds by rememberSaveable {
-        mutableStateOf("")
-    }
+fun InputFields(timerViewModel: TimerViewModel, startButtonState: Boolean) {
+    //region time variables
+    var hours by rememberSaveable { mutableStateOf("00") }
+    var minutes by rememberSaveable { mutableStateOf("00") }
+    var seconds by rememberSaveable { mutableStateOf("00") }
+    //endregion time variables
     Row(
         modifier = Modifier.padding(15.dp)
     ) {
         OutlinedTextField(
             modifier = Modifier.weight(3f),
-            value = inputHours,
-            onValueChange = { inputHours = it },
+            value = hours,
+            onValueChange = {
+                timerViewModel.updateHours(it)
+                hours = timerViewModel.hours.value
+                globH = hours
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            keyboardActions = KeyboardActions(),
             singleLine = true,
+            readOnly = startButtonState,
             label = { Text(text = "Hours") },
-            textStyle = LocalTextStyle.current.copy(textAlign = TextAlign.End)
+            textStyle = LocalTextStyle.current.copy(textAlign = TextAlign.End),
         )
         OutlinedTextField(
             modifier = Modifier.weight(3f),
-            value = inputMinutes,
-            onValueChange = { inputMinutes = it },
+            value = minutes,
+            onValueChange = {
+                timerViewModel.updateMin(it)
+                minutes = timerViewModel.minutes.value
+                globMin = minutes
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
             singleLine = true,
+            readOnly = startButtonState,
             label = { Text(text = "Minutes") },
-            textStyle = LocalTextStyle.current.copy(textAlign = TextAlign.End)
-
+            textStyle = LocalTextStyle.current.copy(textAlign = TextAlign.End),
         )
         OutlinedTextField(
             modifier = Modifier.weight(3f),
-            value = inputSeconds,
-            onValueChange = { inputSeconds = it },
+            value = seconds,
+            onValueChange = {
+                timerViewModel.updateSec(it)
+                seconds = timerViewModel.seconds.value
+                globSec = seconds
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
             singleLine = true,
+            readOnly = startButtonState,
             label = { Text(text = "Seconds") },
-            textStyle = LocalTextStyle.current.copy(textAlign = TextAlign.End)
+            textStyle = LocalTextStyle.current.copy(textAlign = TextAlign.End),
         )
+    }
+}
+
+@Composable
+fun ButtonRow(
+    timerViewModel: TimerViewModel,
+    startButtonState: Boolean,
+    coroutineScope: CoroutineScope
+) {
+    Row() {
+
+        Button(modifier = Modifier
+            .weight(1f)
+            .padding(15.dp),
+            enabled = timerViewModel.checkFinished(),
+            onClick = {
+                timerViewModel.toggleStart()
+                coroutineScope.launch {
+                    timerViewModel.countDown()
+                }
+            }
+        ) {
+            Text(text = if (startButtonState) "Pause" else "Start")
+        }
+        Spacer(modifier = Modifier.width(10.dp))
+        Button(modifier = Modifier
+            .weight(1f)
+            .padding(15.dp),
+            onClick = {
+                timerViewModel.toFalse()
+                timerViewModel.resetCounter(hours = globH, minutes = globMin, seconds = globSec)
+            }) {
+            Text(text = "Reset")
+        }
     }
 }
